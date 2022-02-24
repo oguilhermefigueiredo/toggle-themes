8fc75abfa99b7b80b7766077612dad813f57d22c
Guilherme <negocio.guilhermefigueiredo@gmail.com>
2022-02-24 20:24:33 -0300

    Animations, rotation, behavior

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const toggleBtn = document.querySelector(".toggle-btn")
+const themesContainer = document.querySelector(".themes-container")
 
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark")
+  const currentRotation = parseInt(getComputedStyle(themesContainer).getPropertyValue("--rotation"))
+  themesContainer.style.setProperty("--rotation", currentRotation + 180)
+})
+
+console.log("It's running...")
